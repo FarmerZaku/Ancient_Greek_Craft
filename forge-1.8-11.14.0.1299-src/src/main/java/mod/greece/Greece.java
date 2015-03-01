100b39ff891bc01dcb1bf93fb8b3912b7bf49f6b
Matthew Isom <isom.matt@gmail.com>
2015-02-28 23:44:05 -0600

    Block Rendering
    Got marble and papyrus to render.

--- forge-1.8-11.14.0.1299-src/src/main/java/mod/greece/Greece.java
+++ forge-1.8-11.14.0.1299-src/src/main/java/mod/greece/Greece.java
@@ -13,6 +13,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.init.Items;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
+
 
 @Mod(modid = Greece.MODID, version = Greece.VERSION)
 public class Greece
@@ -26,7 +34,7 @@ public class Greece
 	// BLOCKS
 	public static Block marble = new GreekBlock(Material.rock)
 	.setHardness(0.5F).setStepSound(Block.soundTypeStone)
-	.setUnlocalizedName("marbleBlock").setCreativeTab(CreativeTabs.tabBlock);
+	.setUnlocalizedName("blockMarble").setCreativeTab(CreativeTabs.tabBlock);
 	
 	/*public static Block scroll_shelf = new GreekBlockScrollshelf(Material.wood)
 	.setHardness(0.5F).setStepSound(Block.soundTypeWood)
@@ -35,19 +43,22 @@ public class Greece
 	public static Block papyrusPlantBlock = new GreekBlockPapyrusPlant();
 
 	// ITEMS
-	public static Item papyrusPlantItem = new GreekItemPapyrusPlant(papyrusPlantBlock)
+	/*public static Item papyrusPlantItem = new GreekItemPapyrusPlant(papyrusPlantBlock)
 	.setUnlocalizedName("papyrusPlant").setCreativeTab(CreativeTabs.tabMisc);
 
 	public static Item papyrus = new GreekItem()
-	.setUnlocalizedName("papyrus").setCreativeTab(CreativeTabs.tabMisc);
+	.setUnlocalizedName("papyrus").setCreativeTab(CreativeTabs.tabMisc);*/
 
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		// REGISTER BLOCKS
-		GameRegistry.registerBlock(marble, marble.getUnlocalizedName());
+		// Don't use getUnlocalizedName() here for the block name, apparently it will append like "tile." in front?
+		// or at least I think that's what was breaking it. Anyhow, passing a string works.
+		GameRegistry.registerBlock(marble, "blockMarble");
+		GameRegistry.registerBlock(papyrusPlantBlock, "blockPapyrusPlant");
 		//GameRegistry.registerBlock(scroll_shelf, scroll_shelf.getUnlocalizedName());
-		GameRegistry.registerBlock(papyrusPlantBlock, papyrusPlantBlock.getUnlocalizedName());
+		/*
 
 		// REGISTER ITEMS
 		GameRegistry.registerItem(papyrusPlantItem, papyrusPlantItem.getUnlocalizedName());
@@ -64,12 +75,25 @@ public class Greece
 		GameRegistry.addRecipe(new ItemStack(papyrus, 3), new Object[]{
 			"xxx",
 			'x', papyrusPlantItem
-		});
+		});*/
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		//System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+		//register renders
+		
+    	if(event.getSide() == Side.CLIENT)
+    	{
+    	RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+    
+    	//blocks
+    	renderItem.getItemModelMesher().register(Item.getItemFromBlock(marble), 0, new ModelResourceLocation("greece:blockMarble", "inventory"));
+    	renderItem.getItemModelMesher().register(Item.getItemFromBlock(papyrusPlantBlock), 0, new ModelResourceLocation("greece:blockPapyrusPlant", "inventory"));
+    
+    	//items
+    	//renderItem.getItemModelMesher().register(tutorialItem, 0, new ModelResourceLocation(Reference.MODID + ":" + ((ItemTutorial) tutorialItem).getName(), "inventory"));
+    	}
 	}
 
 	@EventHandler
--- forge-1.8-11.14.0.1299-src/src/main/java/mod/greece/GreekBlockPapyrusPlant.java
+++ forge-1.8-11.14.0.1299-src/src/main/java/mod/greece/GreekBlockPapyrusPlant.java
@@ -5,17 +5,19 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockReed;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class GreekBlockPapyrusPlant extends BlockReed {
 	
 	protected GreekBlockPapyrusPlant() {
         setStepSound(Block.soundTypeGrass);
-        setUnlocalizedName("papyrusPlantBlock");
+        setUnlocalizedName("blockPapyrusPlant");
+        setCreativeTab(CreativeTabs.tabBlock);
     }
 	
 	//@Override
 	public Item getItemDropped(int meta, Random random, int fortune) {
-        return Greece.papyrusPlantItem;
+        return null;//Greece.papyrusPlantItem;
     }
 }
